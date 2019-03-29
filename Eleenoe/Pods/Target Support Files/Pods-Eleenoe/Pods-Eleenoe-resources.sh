1c8f058bee412bb0e629f3268a931ccde52ecc97
zhanglu <zlbfzl@126.com>
2019-03-29 20:07:22 +0800

    增加库

--- Eleenoe/Pods/Target Support Files/Pods-Eleenoe/Pods-Eleenoe-resources.sh
+++ Eleenoe/Pods/Target Support Files/Pods-Eleenoe/Pods-Eleenoe-resources.sh
@@ -3,15 +3,10 @@ set -e
 set -u
 set -o pipefail
 
-function on_error {
-  echo "$(realpath -mq "${0}"):$1: error: Unexpected failure"
-}
-trap 'on_error $LINENO' ERR
-
 if [ -z ${UNLOCALIZED_RESOURCES_FOLDER_PATH+x} ]; then
-  # If UNLOCALIZED_RESOURCES_FOLDER_PATH is not set, then there's nowhere for us to copy
-  # resources to, so exit 0 (signalling the script phase was successful).
-  exit 0
+    # If UNLOCALIZED_RESOURCES_FOLDER_PATH is not set, then there's nowhere for us to copy
+    # resources to, so exit 0 (signalling the script phase was successful).
+    exit 0
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
@@ -102,6 +97,10 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/AXWebViewController/AXWebViewController/AXWebViewController/AXWebViewController.bundle"
   install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_Expression.plist"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_ExpressionImage.plist"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_Expression.bundle"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_Expression.bundle/gifexpression.bundle"
   install_resource "${PODS_ROOT}/SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -110,6 +109,10 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/AXWebViewController/AXWebViewController/AXWebViewController/AXWebViewController.bundle"
   install_resource "${PODS_ROOT}/IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_Expression.plist"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_ExpressionImage.plist"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_Expression.bundle"
+  install_resource "${PODS_ROOT}/MLEmojiLabel/Classes/MLEmoji_Expression.bundle/gifexpression.bundle"
   install_resource "${PODS_ROOT}/SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
 fi
 
