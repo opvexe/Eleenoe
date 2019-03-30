9e88be6ef2e20c7238e2b18fa667dd4e38469371
SHUMIN <tao_shuminx@foxmail.com>
2019-03-30 23:13:34 +0800

    no message

---
+++ Eleenoe/Eleenoe/Class/Common/BaseCollectionView/ELBaseCollectionView.h
@@ -0,0 +1,17 @@
+//
+//  ELBaseCollectionView.h
+//  Eleenoe
+//
+//  Created by FaceBook on 2019/3/30.
+//  Copyright © 2019 FaceBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ELBaseCollectionView : UICollectionView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Eleenoe/Eleenoe/Class/Common/BaseCollectionView/ELBaseCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  ELBaseCollectionView.m
+//  Eleenoe
+//
+//  Created by FaceBook on 2019/3/30.
+//  Copyright © 2019 FaceBook. All rights reserved.
+//
+
+#import "ELBaseCollectionView.h"
+
+@implementation ELBaseCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
