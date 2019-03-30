0d543369089a36f48cb8c0d096ac953dac42b72f
snowlu <zlbfzl@126.com>
2019-03-30 21:55:14 +0800

    1

---
+++ Eleenoe/Eleenoe/Class/Pages/Home/View/ELMyofascialBottomView.h
@@ -0,0 +1,17 @@
+//
+//  ELMyofascialBottomView.h
+//  Eleenoe
+//
+//  Created by HuiLu on 2019/3/30.
+//  Copyright © 2019 FaceBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ELMyofascialBottomView : GMBaseView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Eleenoe/Eleenoe/Class/Pages/Home/View/ELMyofascialBottomView.m
@@ -0,0 +1,13 @@
+//
+//  ELMyofascialBottomView.m
+//  Eleenoe
+//
+//  Created by HuiLu on 2019/3/30.
+//  Copyright © 2019 FaceBook. All rights reserved.
+//
+
+#import "ELMyofascialBottomView.h"
+
+@implementation ELMyofascialBottomView
+
+@end
